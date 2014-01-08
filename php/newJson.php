0fa6997b73e577fd421cb0f494ebcd505b404fc4
Asbjorn <asbjornhp@gmail.com>
2014-01-08 23:29:50 +0000

    php test

--- php/newJson.php
+++ php/newJson.php
@@ -32,7 +32,7 @@ mysql_set_charset("UTF8");
 	$data .='{"items": [';
 	//Load all departments from table
 	$uniqueList = mysql_query("SELECT DISTINCT $uniqueItem $uniqueItem FROM $tableName;");
-	echo $uniqueList
+	echo $uniqueList;
 
 	$resultyfir = mysql_query("SELECT * FROM $tableName ");   
 	echo mysql_error();
