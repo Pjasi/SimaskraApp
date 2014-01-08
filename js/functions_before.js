abccda09029e0c29f03c90fcd696c97c81ce9a1f
Rúnar Kristinsson <runar4@gmail.com>
2014-01-08 16:21:49 +0000

    Removeing the active state on buttons in the class flokkur

--- js/functions_before.js
+++ js/functions_before.js
@@ -7,53 +7,16 @@ $(document).on('mobileinit', function (e) {
 	$.mobile.buttonMarkup.hoverDelay = 0; // (default: 300ms)
 	$.mobile.defaultPageTransition = 'none'; // (default: pop)
 	$.mobile.defaultDialogTransition = 'none'; // (default: pop)
-	$.event.special.swipe.scrollSupressionThreshold = 40;  // (default: 10px) – More than this horizontal displacement, and we will suppress scrolling.
-	$.event.special.swipe.durationThreshold = 1000; // (default: 1000ms) – More time than this, and it isn't a swipe.
-	$.event.special.swipe.horizontalDistanceThreshold = 30; // (default: 30px) – Swipe horizontal displacement must 
-	$.event.special.swipe.verticalDistanceThreshold = 40; // (default: 75px) – Swipe vertical displacement must be less than this.
+	$.event.special.swipe.scrollSupressionThreshold = 40;  // (default: 10px) ï¿½ More than this horizontal displacement, and we will suppress scrolling.
+	$.event.special.swipe.durationThreshold = 1000; // (default: 1000ms) ï¿½ More time than this, and it isn't a swipe.
+	$.event.special.swipe.horizontalDistanceThreshold = 30; // (default: 30px) ï¿½ Swipe horizontal displacement must 
+	$.event.special.swipe.verticalDistanceThreshold = 40; // (default: 75px) ï¿½ Swipe vertical displacement must be less than this.
 });
 
-// device ready
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-	// execute when app resumes from pause
-	document.addEventListener("resume", onResume, false);
-	// execute when app goes to pause (home button or opening other app)
-	document.addEventListener("pause", onPause, false);
-	// override default backbutton behavior with own
-	document.addEventListener("backbutton", pressBackButton, false);
-	// demonstrate panel menu on first boot
-	if (window.localStorage.getItem('firstBoot') === null) {
-		$("#headerTitle" + window.localStorage.getItem("divIdGlobal")).attr("src", "images/icons/ic_launcher_full_menu_selected.png");
-		setTimeout(function () {
-			togglePanel('#panelMenuIndex');
-		}, 500);
-		setTimeout(function () {
-			$("#headerTitle" + window.localStorage.getItem("divIdGlobal")).attr("src", "images/icons/ic_launcher_full_menu.png");
-			togglePanel('#panelMenuIndex');
-		}, 1500);
-		window.localStorage.setItem('firstBoot', 'done');
-	}
-}
 
-// override default back button handling
-function pressBackButton() {
-	// if panel is not open, then go on
-	if (window.localStorage.getItem('panelLeft') === 'closed' && window.localStorage.getItem('panelRight') === 'closed') {
-		if ($.mobile.activePage.is('#indexPage')) {
-			navigator.app.exitApp();
-		} else {
-			window.history.back();
-		}
-	// else close panels first, and stop further action
-	} else {
-		if (window.localStorage.getItem('panelLeft') === 'open') {
-			var divLeftId = '#panelMenu' + window.localStorage.getItem("divIdGlobal");
-			$(divLeftId).panel("close");
-		} else if (window.localStorage.getItem('panelRight') === 'open') {
-			var divRightId = '#panelMenuRight' + window.localStorage.getItem("divIdGlobal");
-			$(divRightId).panel("close");
-		}
-	}
-}
+$(document).bind('mobileinit', function () {
+    $.mobile.activeBtnClass = 'unused';
+ });
+
+
 
