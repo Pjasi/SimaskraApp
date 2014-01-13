ce75c64cf5f04332097d8d1488efb2a2aca70a3f
Rúnar Kristinsson <runar4@gmail.com>
2014-01-13 02:19:22 +0000

    Panel link removed from headers

--- js/update.js
+++ js/update.js
@@ -1,7 +1,4 @@
-﻿//TODO hafa splash screen a tangad til loading er buid
-//Prufu fall til ad birta loading screen a medan vid saekjum gogn
-//Stilling i config .xml setur loading screen sem hatt gildi og tekur svo af tegar ajax er buid
-function Update_Check()
+﻿function Update_Check()
 {
 	$.ajax({
 		url: 'http://entrio.appservicestation.com/lastchange.php?timestamp='+localStorage.timestamp,
