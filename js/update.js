414e2d504128d008888c62a2e6909f467fd04ced
Rúnar Kristinsson <runar4@gmail.com>
2014-01-13 16:18:14 +0000

    Fixing update

--- js/update.js
+++ js/update.js
@@ -7,14 +7,18 @@
 
 			category = $.parseJSON(msg);
 			sessionStorage.timi = category.lastchange.timi;
+            sessionStorage.uppfaera = category.lastchange.uppfaera;
+            console.log(sessionStorage.uppfaera);
             if(sessionStorage.uppfaera == "1") //ta uppfaera nuna alltaf uppfaert
 			//if(sessionStorage.timi > localStorage.timestamp)
 			{
+                console.log("updating");
 				Update();
                 localStorage.timestamp = sessionStorage.timi;
 			}
 			else
 			{
+                console.log("not updating");
 				create_firstpage();
 			}
 		}
